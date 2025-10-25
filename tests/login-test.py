f4626fb4b4f3b3376ebdcbaa90e8d8ef744c00a4
josephpereira17 <josephpereira1913@gmail.com>
2025-10-25 20:40:52 -0300

    Creación del BasePage y mejoras en el test

--- tests/login-test.py
+++ tests/login-test.py
@@ -1,28 +1,31 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
 
-def test_login():
+@pytest.fixture(scope="function")
+def driver_setup():
     driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com/")
-    wait = WebDriverWait(driver, 10)
+    driver.implicitly_wait(5) 
+    yield driver
+    
+    driver.quit()
+
+def test_login(driver_setup):
+    driver = driver_setup
+    base_page = BasePage(driver)
+    base_page.go_to_url("https://www.saucedemo.com/")
 
     # Localizar elementos
-    usuarioTxt = driver.find_element(By.ID,"user-name")
-    claveTxt = driver.find_element(By.ID,"password")
-    loginBtn = driver.find_element(By.ID,"login-button")
+    usuarioTxt = base_page._find_element(By.ID,"user-name")
+    claveTxt = base_page._find_element(By.ID,"password")
+    loginBtn = base_page._find_element(By.ID,"login-button")
 
     usuarioTxt.send_keys("standard_user")
     claveTxt.send_keys("secret_sauce")
     loginBtn.click()
 
-    titulo_productos = wait.until(
-            EC.visibility_of_element_located((By.XPATH, "//span[text()='Products']"))
-        )
+    titulo_productos = base_page._get_element_text(By.XPATH, "//span[text()='Products']")
         
-    assert titulo_productos.text == "Products", "El título de la página no es 'Products'. Login fallido."
+    assert titulo_productos == "Products", "El título de la página no es 'Products'. Login fallido."
     print("Login exitoso - La validación de título correcta")
-
-    driver.quit()
